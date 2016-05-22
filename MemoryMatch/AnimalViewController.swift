8a08ad9f3348858375f7514a8070ba452bb71ce9
Lucas Huet-Hudson <lucas.huet.hudson@gmail.com>
2016-05-22 11:57:13 -0700

    refactored animal controller

--- MemoryMatch/AnimalViewController.swift
+++ MemoryMatch/AnimalViewController.swift
@@ -9,18 +9,11 @@
 import UIKit
 
 class AnimalViewController: UIViewController {
-    var count = Int()
-    var player = Int()
-    var turn = Int()
-    var delegate = UIViewController()
-    var cardValuesDrawn = [Int]()
-    var playerOneScore = Int()
-    var playerTwoScore = Int()
-    var score = String()
-    var animalsDeck = [Card]()
-    var imagesArray = [UIImageView]()
-    var recognizersArray = [UIGestureRecognizer]()
-    var actionsArray = [String]()
+    
+    //--------------------------------------------------
+    // MARK: - Constants
+    //--------------------------------------------------
+    
     let alligator = Card(flippedCard: UIImage(named: "Alligator-48")!, unflippedCard: UIImage(named: "PurpleQ")!, value: 1, name: "Alligator")
     let bear = Card(flippedCard: UIImage(named: "Bear-48")!, unflippedCard: UIImage(named: "PurpleQ")!, value: 2, name: "Bear")
     let butterfly = Card(flippedCard: UIImage(named: "Butterfly-48")!, unflippedCard: UIImage(named: "PurpleQ")!, value: 3, name: "Butterfly")
@@ -36,6 +29,28 @@ class AnimalViewController: UIViewController {
     let horse = Card(flippedCard: UIImage(named: "Horse-48")!, unflippedCard: UIImage(named: "PurpleQ")!, value: 13, name: "Horse")
     let unicorn = Card(flippedCard: UIImage(named: "Unicorn-48")!, unflippedCard: UIImage(named: "PurpleQ")!,value: 14, name: "Unicorn")
     let wolf = Card(flippedCard: UIImage(named: "Wolf-48")!, unflippedCard: UIImage(named: "PurpleQ")!, value: 15, name: "Wolf")
+    
+    //--------------------------------------------------
+    // MARK: - Variables
+    //--------------------------------------------------
+    
+    var count = Int()
+    var player = Int()
+    var turn = Int()
+    var delegate = UIViewController()
+    var cardValuesDrawn = [Int]()
+    var playerOneScore = Int()
+    var playerTwoScore = Int()
+    var score = String()
+    var animalsDeck = [Card]()
+    var imagesArray = [UIImageView]()
+    var recognizersArray = [UIGestureRecognizer]()
+    var actionsArray = [String]()
+    
+    //--------------------------------------------------
+    // MARK: - Outlets
+    //--------------------------------------------------
+    
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var image1: UIImageView!
     @IBOutlet weak var image2: UIImageView!
@@ -68,6 +83,17 @@ class AnimalViewController: UIViewController {
     @IBOutlet weak var image29: UIImageView!
     @IBOutlet weak var image30: UIImageView!
     @IBOutlet weak var winnerLabel: UILabel!
+    
+    //--------------------------------------------------
+    // MARK: - View Lifecycle
+    //--------------------------------------------------
+    
+    
+    
+    //--------------------------------------------------
+    // MARK: - Actions
+    //--------------------------------------------------
+    
     @IBAction func resetButtonPressed(sender: UIButton) {
         initializeLabelsAndCards()
         winnerLabel.hidden = true
@@ -76,7 +102,7 @@ class AnimalViewController: UIViewController {
         }
     }
     @IBAction func menuButtonPressed(sender: UIButton) {
-        delegate.dismissViewControllerAnimated(true) { 
+        delegate.dismissViewControllerAnimated(true) {
         }
     }
     @IBOutlet weak var animalLabel: UILabel!
@@ -85,31 +111,11 @@ class AnimalViewController: UIViewController {
         initializeLabelsAndCards()
         winnerLabel.hidden = true
     }
-    func initializeLabelsAndCards() {
-        assignbackground()
-        resetDeck()
-        initializeImagesArray()
-        setRecognizers()
-        player = 1
-        turn = 1
-        playerOneScore = 0
-        playerTwoScore = 0
-        count = 0
-        score = ("\(playerOneScore) - \(playerTwoScore)")
-        scoreLabel.text = score
-        animalLabel.text = ""
-    }
-    func assignbackground(){
-        let background = UIImage(named: "Mountains")
-        var imageView : UIImageView!
-        imageView = UIImageView(frame: view.bounds)
-        imageView.contentMode =  UIViewContentMode.ScaleAspectFill
-        imageView.clipsToBounds = true
-        imageView.image = background
-        imageView.center = view.center
-        view.addSubview(imageView)
-        self.view.sendSubviewToBack(imageView)
-    }
+
+    //--------------------------------------------------
+    // MARK: - Helper Functions
+    //--------------------------------------------------
+    
     func resetDeck(){
         animalsDeck = [Card]()
         animalsDeck = [alligator, bear, butterfly, elephant, giraffe, gorilla, hummingbird, kangaroo, ladybug, leopard, panda, snake, horse, unicorn, wolf, alligator, bear, butterfly, elephant, giraffe, gorilla, hummingbird, kangaroo, ladybug, leopard, panda, snake, horse, unicorn, wolf]
@@ -120,51 +126,22 @@ class AnimalViewController: UIViewController {
             }
         }
     }
-    func initializeImagesArray(){
-        imagesArray = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13, image14, image15, image16, image17, image18, image19, image20, image21, image22, image23, image24, image25, image26, image27, image28, image29, image30]
-        for i in 0...imagesArray.count - 1 {
-            imagesArray[i].image = animalsDeck[i].unflippedCard
+
+    func printText(player: Int){
+        self.view.bringSubviewToFront(winnerLabel);
+        animalLabel.hidden = true
+        winnerLabel.hidden = false
+        if player == 1 {
+            winnerLabel.text = "Player 1 Wins"
+        } else {
+            winnerLabel.text = "Player 2 Wins"
         }
-    }
-    func setRecognizers(){
-        let recognizer1 = UITapGestureRecognizer()
-        let recognizer2 = UITapGestureRecognizer()
-        let recognizer3 = UITapGestureRecognizer()
-        let recognizer4 = UITapGestureRecognizer()
-        let recognizer5 = UITapGestureRecognizer()
-        let recognizer6 = UITapGestureRecognizer()
-        let recognizer7 = UITapGestureRecognizer()
-        let recognizer8 = UITapGestureRecognizer()
-        let recognizer9 = UITapGestureRecognizer()
-        let recognizer10 = UITapGestureRecognizer()
-        let recognizer11 = UITapGestureRecognizer()
-        let recognizer12 = UITapGestureRecognizer()
-        let recognizer13 = UITapGestureRecognizer()
-        let recognizer14 = UITapGestureRecognizer()
-        let recognizer15 = UITapGestureRecognizer()
-        let recognizer16 = UITapGestureRecognizer()
-        let recognizer17 = UITapGestureRecognizer()
-        let recognizer18 = UITapGestureRecognizer()
-        let recognizer19 = UITapGestureRecognizer()
-        let recognizer20 = UITapGestureRecognizer()
-        let recognizer21 = UITapGestureRecognizer()
-        let recognizer22 = UITapGestureRecognizer()
-        let recognizer23 = UITapGestureRecognizer()
-        let recognizer24 = UITapGestureRecognizer()
-        let recognizer25 = UITapGestureRecognizer()
-        let recognizer26 = UITapGestureRecognizer()
-        let recognizer27 = UITapGestureRecognizer()
-        let recognizer28 = UITapGestureRecognizer()
-        let recognizer29 = UITapGestureRecognizer()
-        let recognizer30 = UITapGestureRecognizer()
-        recognizersArray = [recognizer1, recognizer2, recognizer3, recognizer4, recognizer5, recognizer6, recognizer7, recognizer8, recognizer9, recognizer10, recognizer11, recognizer12, recognizer13, recognizer14, recognizer15, recognizer16, recognizer17, recognizer18, recognizer19, recognizer20, recognizer21, recognizer22, recognizer23, recognizer24, recognizer25, recognizer26, recognizer27, recognizer28, recognizer29, recognizer30]
-        actionsArray = ["image1HasBeenTapped", "image2HasBeenTapped", "image3HasBeenTapped", "image4HasBeenTapped", "image5HasBeenTapped", "image6HasBeenTapped", "image7HasBeenTapped", "image8HasBeenTapped", "image9HasBeenTapped", "image10HasBeenTapped", "image11HasBeenTapped", "image12HasBeenTapped", "image13HasBeenTapped", "image14HasBeenTapped", "image15HasBeenTapped", "image16HasBeenTapped", "image17HasBeenTapped", "image18HasBeenTapped", "image19HasBeenTapped", "image20HasBeenTapped", "image21HasBeenTapped", "image22HasBeenTapped", "image23HasBeenTapped", "image24HasBeenTapped", "image25HasBeenTapped", "image26HasBeenTapped", "image27HasBeenTapped", "image28HasBeenTapped", "image29HasBeenTapped", "image30HasBeenTapped"]
-        for i in 0...imagesArray.count-1 {
-            imagesArray[i].userInteractionEnabled = true
-            imagesArray[i].addGestureRecognizer(recognizersArray[i])
-            recognizersArray[i].addTarget(self, action: Selector(actionsArray[i]))
+        for i in 0...imagesArray.count-1{
+            imagesArray[i].hidden = true
         }
+        
     }
+    
     func fadeFirst(image: UIImageView, number: Int){
         image.fadeOut(completion: {
             (finished: Bool) -> Void in
@@ -177,6 +154,7 @@ class AnimalViewController: UIViewController {
             
         })
     }
+    
     func fadeSecondWithAlert(image: UIImageView, number: Int){
         image.fadeOutWithDelay(completion: {
             (finished: Bool) -> Void in
@@ -202,6 +180,7 @@ class AnimalViewController: UIViewController {
             })
         })
     }
+    
     func fadeSecond(image: UIImageView, number: Int){
         image.fadeOutWithDelay(completion: {
             (finished: Bool) -> Void in
@@ -212,20 +191,7 @@ class AnimalViewController: UIViewController {
             })
         })
     }
-    func printText(player: Int){
-        self.view.bringSubviewToFront(winnerLabel);
-        animalLabel.hidden = true
-        winnerLabel.hidden = false
-        if player == 1 {
-            winnerLabel.text = "Player 1 Wins"
-        } else {
-            winnerLabel.text = "Player 2 Wins"
-        }
-        for i in 0...imagesArray.count-1{
-            imagesArray[i].hidden = true
-        }
-        
-    }
+    
     func update(number: Int){
         if turn == 1 {
             cardValuesDrawn.append(animalsDeck[number-1].value)
@@ -283,7 +249,95 @@ class AnimalViewController: UIViewController {
             }
         }
     }
+}
 
+//--------------------------------------------------
+// MARK: - Initializers
+//--------------------------------------------------
+
+extension AnimalViewController {
+    
+    func initializeLabelsAndCards() {
+        assignbackground()
+        resetDeck()
+        initializeImagesArray()
+        setRecognizers()
+        player = 1
+        turn = 1
+        playerOneScore = 0
+        playerTwoScore = 0
+        count = 0
+        score = ("\(playerOneScore) - \(playerTwoScore)")
+        scoreLabel.text = score
+        animalLabel.text = ""
+    }
+    
+    func assignbackground(){
+        let background = UIImage(named: "Mountains")
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIViewContentMode.ScaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+    }
+    
+    func initializeImagesArray(){
+        imagesArray = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13, image14, image15, image16, image17, image18, image19, image20, image21, image22, image23, image24, image25, image26, image27, image28, image29, image30]
+        for i in 0...imagesArray.count - 1 {
+            imagesArray[i].image = animalsDeck[i].unflippedCard
+        }
+    }
+}
+
+//--------------------------------------------------
+// MARK: - Gestures
+//--------------------------------------------------
+
+extension AnimalViewController {
+    
+    func setRecognizers(){
+        let recognizer1 = UITapGestureRecognizer()
+        let recognizer2 = UITapGestureRecognizer()
+        let recognizer3 = UITapGestureRecognizer()
+        let recognizer4 = UITapGestureRecognizer()
+        let recognizer5 = UITapGestureRecognizer()
+        let recognizer6 = UITapGestureRecognizer()
+        let recognizer7 = UITapGestureRecognizer()
+        let recognizer8 = UITapGestureRecognizer()
+        let recognizer9 = UITapGestureRecognizer()
+        let recognizer10 = UITapGestureRecognizer()
+        let recognizer11 = UITapGestureRecognizer()
+        let recognizer12 = UITapGestureRecognizer()
+        let recognizer13 = UITapGestureRecognizer()
+        let recognizer14 = UITapGestureRecognizer()
+        let recognizer15 = UITapGestureRecognizer()
+        let recognizer16 = UITapGestureRecognizer()
+        let recognizer17 = UITapGestureRecognizer()
+        let recognizer18 = UITapGestureRecognizer()
+        let recognizer19 = UITapGestureRecognizer()
+        let recognizer20 = UITapGestureRecognizer()
+        let recognizer21 = UITapGestureRecognizer()
+        let recognizer22 = UITapGestureRecognizer()
+        let recognizer23 = UITapGestureRecognizer()
+        let recognizer24 = UITapGestureRecognizer()
+        let recognizer25 = UITapGestureRecognizer()
+        let recognizer26 = UITapGestureRecognizer()
+        let recognizer27 = UITapGestureRecognizer()
+        let recognizer28 = UITapGestureRecognizer()
+        let recognizer29 = UITapGestureRecognizer()
+        let recognizer30 = UITapGestureRecognizer()
+        recognizersArray = [recognizer1, recognizer2, recognizer3, recognizer4, recognizer5, recognizer6, recognizer7, recognizer8, recognizer9, recognizer10, recognizer11, recognizer12, recognizer13, recognizer14, recognizer15, recognizer16, recognizer17, recognizer18, recognizer19, recognizer20, recognizer21, recognizer22, recognizer23, recognizer24, recognizer25, recognizer26, recognizer27, recognizer28, recognizer29, recognizer30]
+        actionsArray = ["image1HasBeenTapped", "image2HasBeenTapped", "image3HasBeenTapped", "image4HasBeenTapped", "image5HasBeenTapped", "image6HasBeenTapped", "image7HasBeenTapped", "image8HasBeenTapped", "image9HasBeenTapped", "image10HasBeenTapped", "image11HasBeenTapped", "image12HasBeenTapped", "image13HasBeenTapped", "image14HasBeenTapped", "image15HasBeenTapped", "image16HasBeenTapped", "image17HasBeenTapped", "image18HasBeenTapped", "image19HasBeenTapped", "image20HasBeenTapped", "image21HasBeenTapped", "image22HasBeenTapped", "image23HasBeenTapped", "image24HasBeenTapped", "image25HasBeenTapped", "image26HasBeenTapped", "image27HasBeenTapped", "image28HasBeenTapped", "image29HasBeenTapped", "image30HasBeenTapped"]
+        for i in 0...imagesArray.count-1 {
+            imagesArray[i].userInteractionEnabled = true
+            imagesArray[i].addGestureRecognizer(recognizersArray[i])
+            recognizersArray[i].addTarget(self, action: Selector(actionsArray[i]))
+        }
+    }
+    
     func image1HasBeenTapped(){
         fadeFirst(image1, number: 1)
     }
@@ -375,40 +429,13 @@ class AnimalViewController: UIViewController {
         fadeFirst(image30, number: 30)
     }
 }
-//--------------------------------------------------
-// MARK: - Constants
-//--------------------------------------------------
-
-//--------------------------------------------------
-// MARK: - Variables
-//--------------------------------------------------
-
-//--------------------------------------------------
-// MARK: - Outlets
-//--------------------------------------------------
-
-//--------------------------------------------------
-// MARK: - View Lifecycle
-//--------------------------------------------------
 
 
-//--------------------------------------------------
-// MARK: - Actions
-//--------------------------------------------------
 
 
-//--------------------------------------------------
-// MARK: - Segues
-//--------------------------------------------------
 
 
-//--------------------------------------------------
-// MARK: - Helper Functions
-//--------------------------------------------------
 
 
-//--------------------------------------------------
-// MARK: - Go Back Delegate
-//--------------------------------------------------
 
 
